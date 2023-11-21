b0be86e343f8760233f53fcb16f7122308ed0602
uwais67 <114695768+uwais67@users.noreply.github.com>
2023-11-21 11:14:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from  'path';
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
@@ -17,6 +18,8 @@ connectDB();
 //rest object
 const app = express();
 
+const ___filename= fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 //middelwares
 app.use(cors());
 app.use(express.json());
